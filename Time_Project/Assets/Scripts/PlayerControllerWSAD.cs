6f5206ed174a8cc4c79360d4ba74a0d87fd074e5
sag754 <56234238+sag754@users.noreply.github.com>
2019-11-14 00:00:04 -0500

    Created Level space, WASD controller script, Third Person Camera Script, and Audio resource.

---
+++ Time_Project/Assets/Scripts/PlayerControllerWSAD.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerWSAD : MonoBehaviour
+{
+    Rigidbody rb; //declare a reference for the rigidbody
+
+    public GameObject camera;
+    public float force = 100.0f; //create a force to push the playerObject
+    bool isSmall = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); //get the rigidbody from this playerObject
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool hasInput = false; //create a local variable to track whether the user has inputed something
+
+        if (Input.GetKey(KeyCode.W))
+        { //if the W is pressed
+            Vector3 cameraforward = camera.transform.forward;
+            cameraforward.y = 0;
+            cameraforward = Vector3.Normalize(cameraforward);
+            rb.AddForce(cameraforward * force);
+            hasInput = true; //the user has pressed a key
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        { //if the S is pressed
+            Vector3 cameraforward = camera.transform.forward;
+            cameraforward.y = 0;
+            cameraforward = Vector3.Normalize(cameraforward);
+            rb.AddForce(cameraforward * -force);
+            hasInput = true; //the user has pressed a key
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        { //if the A is pressed
+            Vector3 cameraright = camera.transform.right;
+            cameraright.y = 0;
+            cameraright = Vector3.Normalize(cameraright);
+            rb.AddForce(cameraright * -force);
+            hasInput = true; //the user has pressed a key
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {//if the D is pressed
+            Vector3 cameraright = camera.transform.right;
+            cameraright.y = 0;
+            cameraright = Vector3.Normalize(cameraright);
+            rb.AddForce(cameraright * force);
+            hasInput = true; //the user has pressed a key
+        }
+
+        if (!Input.anyKey)
+        { //if the user hasn't pressed a key
+            rb.velocity = rb.velocity * 1.0f; //decrease velocity
+        }
+
+    }
+}
---
+++ Time_Project/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour  // create Third Person Camera Class
+{
+    public Transform lookAt;                   //allows to set what the camera will look like
+    public Transform camTransform;
+
+    private Camera cam;                        //references the camera
+
+    private float distance = 10.0f;            //sets the distance away from the lookAt object
+    private float currentX = 0.0f;             //sets the current X position of the camera
+    private float currentY = 50.0f;             //sets the current Y position of the camera
+    private float sensitivityX = 8f;         //sets how quickly the camera rotates on the X-axis
+    private float sensitivityY = 5f;         //sets how quickly the camera rotates on the Y-axis
+
+    private void Start()
+    {
+        camTransform = transform;
+        cam = Camera.main;                     // Gets the camera on the scene
+    }
+
+    private void Update()
+    {
+        currentX += Input.GetAxis("Mouse X") * sensitivityX;     //sets camera control on the X-axis to the Mouse X and sensitivity
+        currentY -= Input.GetAxis("Mouse Y") * sensitivityY;     //sets camera control on the Y-axis to the Mouse Y and sensitivity
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);                      //updates and changes the current camera position based on X, Y, and Z minus set distance
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);   //maintains the LookAt target as the center point for the camera
+        camTransform.position = lookAt.position + rotation * dir;        //updates the rotation and position of the camera based off the lookAt target's center point
+        camTransform.LookAt(lookAt.position);                            //updates the position of the LookAt target
+    }
+}
